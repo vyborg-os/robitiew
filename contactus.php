2035e7f3c2c28f9dc19531376063daea896ff760
vyborg-os <otokitivictor@gmail.com>
2023-04-21 10:53:11 +0100

    updated committed

---
+++ contactus.php
@@ -0,0 +1,110 @@
+<?php include_once('header.php'); ?>
+
+
+<section class="bread-crumb-section">
+        <img class="shape shape1" src="assets/images/bread/1.png" alt="images_not_found">
+        <img class="shape shape2" src="assets/images/bread/2.png" alt="images_not_found">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2 class="title text-center">Contact Us</h2>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb justify-content-center">
+                            <li class="breadcrumb-item"><a href="./">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page"><span>Contact</span></li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- bread crumb section end -->
+
+    <!-- contact section start -->
+    <section class="contact-section-page section-pt-150 section-pb-150">
+        <div class="container">
+            <div class="row g-0">
+                <div class="col-12">
+                    <div class="contact-form-card">
+                        <div class="row mb-n7">
+                            <div class="col-lg-6 mb-7">
+                                <div class="section-title">
+                                    <h3 class="title" style="color: #CCE00E;">Get In Touch.</h3>
+                                    <span class="comment-hr mb-0"></span>
+                                </div>
+                                <!-- contact media -->
+                                <div class="contact-media contact-media-list mt-xl-8">
+                                    <div class="icon">
+                                        <span class="icofont-ui-call"></span>
+                                    </div>
+                                    <div class="content">
+                                        <span class="text" style="color: #CCE00E;">Phone:</span>
+                                        <a href="tel:07069458940" class="number">(+234) 706 945 8940</a>
+                                    </div>
+                                </div>
+                                <!-- contact media end -->
+                                <!-- contact media -->
+                                <div class="contact-media contact-media-list">
+                                    <div class="icon">
+                                        <span class="icofont-send-mail"></span>
+                                    </div>
+                                    <div class="content">
+                                        <span class="text" style="color: #CCE00E;">Email:</span>
+                                        <a href="mailto:robitiew@gmail.com" class="number">robitiew@gmail.com</a>
+                                    </div>
+                                </div>
+                                <!-- contact media end -->
+                                <!-- contact media -->
+                                <div class="contact-media contact-media-list">
+                                    <div class="icon">
+                                        <span class="icofont-map-pins"></span>
+                                    </div>
+                                    <div class="content">
+                                        <span class="text" style="color: #CCE00E;">Address:</span>
+                                        <h3 class="number">Plot 464 SAMUEL OKAFOR STREET, KUBWA, ABUJA.</h3>
+                                    </div>
+                                </div>
+                                <!-- contact media end -->
+                            </div>
+                            <div class="col-lg-6 mb-7">
+                                <div class="section-title">
+                                    <h3 class="title" style="color: #CCE00E;">Send Us A Message</h3>
+                                    <span class="comment-hr mb-0"></span>
+                                </div>
+
+                                <div class="comment-form pt-xl-8">
+                                    <form class="row gx-4" id="contactForm" action="#" method="POST">
+                                        <div class="col-12 col-sm-6">
+                                            <input class="form-control" placeholder="Enter Your Name" type="text" name="name" />
+                                        </div>
+                                        <div class="col-12 col-sm-6">
+                                            <input class="form-control" placeholder="Enter Your Email" type="text" name="email" />
+                                        </div>
+                                        <div class="col-12">
+                                            <textarea placeholder="Type your question" class="form-control textarea-control" name="message" id="textarea" cols="30" rows="10"></textarea>
+                                        </div>
+                                        <div class="col-12">
+                                            <button type="submit" class="btn btn-success" style="color: #CCE00E;">
+                                                Submit
+                                                <i class="icofont-rounded-double-right"></i>
+                                            </button>
+                                        </div>
+                                    </form>
+                                    <p class="form-message"></p>
+                                </div>
+                            </div>
+                            <div class="col-12 mb-7">
+                            <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3938.8511136348698!2d7.312728614787666!3d9.167900493424593!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104dd9043995cf69%3A0x1dc11fa24f7b2758!2sProf%20Samuel%20O%20Okafor%20Cres%2C%20Kubwa%20901101%2C%20Abuja%2C%20Federal%20Capital%20Territory!5e0!3m2!1sen!2sng!4v1681912058194!5m2!1sen!2sng" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+                                <iframe class="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3938.8511136348698!2d7.312728614787666!3d9.167900493424593!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104dd9043995cf69%3A0x1dc11fa24f7b2758!2sProf%20Samuel%20O%20Okafor%20Cres%2C%20Kubwa%20901101%2C%20Abuja%2C%20Federal%20Capital%20Territory!5e0!3m2!1sen!2sng!4v1681912058194!5m2!1sen!2sng"></iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+<?php include_once('footer.php'); ?>
